8bcfa3d777acbc363d95d6573644fae5329f158f
Sani Sani <sanjin_i@hotmail.com>
2018-01-24 15:41:19 +0100

    Changed this bitch too

--- TestProject/src/com/sani/test/Test.java
+++ TestProject/src/com/sani/test/Test.java
@@ -1,11 +1,17 @@
 package com.sani.test;
 
+import com.sani.demo.Person;
+
 public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("changed from stuff");
 		System.out.println("Hello GITHUB #2");
+		
 		System.out.println();
+		
+		Person pers = new Person();
+		pers.print();
 	}
 
 }
