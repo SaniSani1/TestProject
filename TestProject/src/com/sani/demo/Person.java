c0ba5dacc08d5ec826dba8f810f0397aa57ef31a
Sani Sani <sanjin_i@hotmail.com>
2018-01-24 15:39:03 +0100

    its time to change that shit

--- TestProject/src/com/sani/demo/Person.java
+++ TestProject/src/com/sani/demo/Person.java
@@ -1,6 +1,9 @@
 package com.sani.demo;
 
 public class Person {
+	
+	public void print() {
       System.out.println("Changed this mfacka on GITHUB and hope to get over git bash loclaly");
 	  System.out.println("Changed file in notepad++");
+	}
 }
